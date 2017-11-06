9e8085c3e58db9a72ddfab4768479d9ce29b1c94
chris <chris.shyi@mail.utoronto.ca>
2017-11-06 18:55:18 -0500

    Started working on DirectoryManagerTest

--- phase1/src/test/DirectoryManagerTest.java
+++ phase1/src/test/DirectoryManagerTest.java
@@ -1,18 +1,43 @@
 package test;
 
+import main.DirectoryManager;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DirectoryManagerTest {
+    DirectoryManager manager;
     @org.junit.jupiter.api.BeforeEach
-    void setUp() {
+    void setUp() throws IOException {
+        String testDir = "/h/u7/c7/05/shyichin/csc207/groupProject/group_0485/phase1/src/test";
+        Path subDir = Paths.get(testDir + "/sub");
+        Files.createDirectory(subDir);
+
+        File imgInTestDir = new File(testDir + "/test1.jpg");
+        imgInTestDir.createNewFile();
+
+        File imgInSubDir = new File(subDir.toString() + "/test2.jpg");
+        imgInSubDir.createNewFile();
+        manager = new DirectoryManager()
     }
 
     @org.junit.jupiter.api.Test
     void getImagesUnderRoot() {
+
     }
 
     @org.junit.jupiter.api.Test
     void getAllImagesUnderRoot() {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    void tearDown() {
+
     }
 
 }
