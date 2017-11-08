d6c916aa70df1fdfdf46b53a3aca60c45c09391e
Chris-T470 <chrisshyi13@gmail.com>
2017-11-08 17:00:42 -0500

    Started working on the FX.Controller class. Created constructor and some instance variables

--- phase1/src/FX/Controller.java
+++ phase1/src/FX/Controller.java
@@ -1,4 +1,30 @@
 package FX;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
 public class Controller {
+
+    // Use the UI element id specified in the FXML file as the variable name to get a hook on those elements
+    @FXML
+    Button chooseDirButton;
+
+    @FXML
+    Button moveFileButton;
+
+    @FXML
+    Button openCurDirButton;
+
+    public Controller() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("layout.fxml"));
+        loader.setController(this);
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
