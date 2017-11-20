77324a3f95c66b4b0a59c4a6a6b65660210a2911
Kevin Mok <kevin.mok@live.ca>
2017-11-19 22:59:21 -0500

    Added javadoc.

--- phase1/src/fx/Main.java
+++ phase1/src/fx/Main.java
@@ -15,6 +15,9 @@ import java.io.IOException;
 public class Main extends Application {
     private ImageTagManager imgTagManager = ImageTagManager.getInstance();
 
+    /**
+     * Method from which our program starts.
+     */
     public static void main(String[] args) {
         launch(args);
     }
