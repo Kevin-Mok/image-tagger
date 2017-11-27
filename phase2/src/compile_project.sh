bfbe15d2bb6cbe9e489f10695749c1fcd1184999
chris <chris.shyi@mail.utoronto.ca>
2017-11-26 19:10:47 -0500

    Fix shell script to compile/execute application
    Resolved issue #23. The application can now be compiled and executed from the command line by
    entering "./src/compile_project.sh" from the phase2 directory.

--- phase2/src/compile_project.sh
+++ phase2/src/compile_project.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Execute from the phase1 directory
+# Execute from the phase2 directory
 rm -rf out/class_files
 rm -f sources.txt
 find -name "*.java" >sources.txt
 mkdir -p out/class_files
-javac -classpath /local/packages/idea-IC-172.4155.36/plugins/junit/lib/junit-jupiter-api-5.0.0.jar:/local/packages/idea-IC-172.4155.36/plugins/junit/lib/opentest4j-1.0.0.jar:"lib/*":lib/retrofit-2.2.0.jar @sources.txt -d out/class_files/
+javac -classpath "/local/packages/idea-IC-172.4155.36/plugins/junit/lib/junit-jupiter-api-5.0.0.jar:/local/packages/idea-IC-172.4155.36/plugins/junit/lib/opentest4j-1.0.0.jar:lib/*" @sources.txt -d out/class_files/
 cp -a src/fx/resources/ out/class_files/fx/
 cp src/fx/layout.fxml out/class_files/fx/
-java -cp out/class_files fx.Main
+java -cp out/class_files:lib/*  fx.Main
